7f078fd4bf142366ae647e10aa3b3b22fe4cd5cb
Yash Agarwal <agrwal.ysh94@gmail.com>
2017-01-15 19:20:02 +0530

    First Comit

---
+++ data_structures/linked_lists/insert_node.cpp
@@ -0,0 +1,27 @@
+//Insert a node at the head of a linked list
+
+/*
+  Insert Node at the begining of a linked list
+  Initially head pointer argument could be NULL for empty list
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+return back the pointer to the head of the linked list in the below method.
+*/
+Node* Insert(Node *head,int data)
+{
+  // Complete this method
+    Node* N = new Node();
+
+    N->next = head;
+    head = N;
+    N->data = data;
+
+    return head;
+}
+
+
+//Insert a node at the head of a linked list
